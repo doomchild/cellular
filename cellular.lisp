4b0f09c1932f95ada3f608c99971ae10c9c3c67d
Lee Crabtree <lee.crabtree@gmail.com>
2013-05-29 00:06:17 -0500

    updated to use fps-fixed timestep only make the sdl:rectangle object once per update

--- cellular.lisp
+++ cellular.lisp
@@ -8,17 +8,20 @@
 (defparameter *main-buffer* (make-array (* *buffer-x* *buffer-y*) :initial-element 0 :element-type 'integer))
 (defparameter *secondary-buffer* (make-array (* *buffer-x* *buffer-y*) :initial-element 0 :element-type 'integer))
 
+(defparameter *run-simulation* t)
+
 (defun element-at (buf x y)
   (aref buf (+ (* x *buffer-y*) y)))
 
 (defun (setf element-at) (value buf x y)
   (setf (aref buf (+ (* x *buffer-y*) y)) value))
 
-(setf (element-at *main-buffer* 10 5) 1)
-(setf (element-at *main-buffer* 11 5) 1)
-(setf (element-at *main-buffer* 12 5) 1)
-(setf (element-at *main-buffer* 13 5) 1)
-(setf (element-at *main-buffer* 14 5) 1)
+(setf (element-at *main-buffer* 10 10) 1
+      (element-at *main-buffer* 11 10) 1
+      (element-at *main-buffer* 12 10) 1
+      (element-at *main-buffer* 13 10) 1
+      (element-at *main-buffer* 14 10) 1
+      (element-at *main-buffer* 15 10) 1)
 
 (defun get-neighbor-coords (x y)
   (let ((mx (1- x)) (my (1- y)) (px (1+ x)) (py (1+ y)))
@@ -52,28 +55,36 @@
 
 (defun run ()
   (sdl:with-init ()
-    (sdl:window 600 600 :fps (make-instance 'sdl:fps-fixed :target-frame-rate 1))
+    (sdl:window 600 600 :fps (make-instance 'sdl:fps-timestep :dt 500))
     
     (sdl:with-events ()
       (:quit-event () t)
       (:key-down-event (:key key)
 		       (when (sdl:key= key :sdl-key-escape)
-			 (sdl:push-quit-event)))
+			 (sdl:push-quit-event))
+		       (when (sdl:key= key :sdl-key-return)
+			 (if (eq *run-simulation* t)
+			     (setf *run-simulation* nil)
+			     (setf *run-simulation* t))))
       (:video-expose-event () (sdl:update-display))
       (:idle ()
 	     (sdl:clear-display sdl:*black*)
-	     
-	     (process-cells *main-buffer* *secondary-buffer*)
-	     (rotatef *main-buffer* *secondary-buffer*)
+
+	     (sdl:with-timestep
+	       (when (eq *run-simulation* t)
+		 (progn
+		   (process-cells *main-buffer* *secondary-buffer*)
+		   (rotatef *main-buffer* *secondary-buffer*))))
 
 	     (draw-buffer *main-buffer*)
 
 	     (sdl:update-display)))))
 
 (defun draw-buffer (buffer)
-  (loop for y upto (1- *buffer-y*) do
-       (loop for x upto (1- *buffer-x*) do
-	    (let ((r (sdl:rectangle :x (* x *rect-w*) :y (* y *rect-h*) :w *rect-w* :h *rect-h*)))
+  (let ((r (sdl:rectangle :x 0 :y 0 :w *rect-w* :h *rect-h*)))
+    (loop for y upto (1- *buffer-y*) do
+	 (loop for x upto (1- *buffer-x*) do
+	      (sdl:set-rectangle-* r :x (* x *rect-w*) :y (* y *rect-h*))
 	      (if (> (element-at buffer x y) 0)
 		  (sdl:draw-box r :color sdl:*black*)
 		  (sdl:draw-box r :color sdl:*white* :stroke-color sdl:*black*))))))
